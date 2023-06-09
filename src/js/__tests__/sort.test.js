b8f6559efdb49730eaeb8a4708d532a761cb7b5c
kkk <kutimskyi@gmail.com>
2023-06-09 13:36:46 +0300

    fixed lint problems

--- src/js/__tests__/sort.test.js
+++ src/js/__tests__/sort.test.js
@@ -1,11 +1,12 @@
 import sortByHealth from '../sort';
+
 const players = [
-  {name: 'мечник', health: 10},
-  {name: 'маг', health: 100},
-  {name: 'лучник', health: 80},
-]
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
 test('players', () => {
   const result = sortByHealth(players);
-  
-  expect(result).toEqual('critical');
+  const expected = players.sort((a, b) => b.health - a.health);
+  expect(result).toEqual(expected);
 });
--- src/js/sort.js
+++ src/js/sort.js
@@ -1,5 +1,3 @@
-export default function sortByHealth (players) {
- return players.sort((a,b) => {
-    return b.health - a.health;
-})
+export default function sortByHealth(players) {
+  return players.sort((a, b) => b.health - a.health);
 }
